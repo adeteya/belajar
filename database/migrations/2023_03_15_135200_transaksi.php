4946c1cfa91525c5a4f98db5429818314c4ba1fc
adeteya <aditpermanaputra75@gmail.com>
2023-03-15 20:55:10 +0700

    Menambahkan Semua Database

---
+++ database/migrations/2023_03_15_135200_transaksi.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('appointment_id');
+            $table->string('fee_doctor')->nullable();
+            $table->string('fee_spesialist')->nullable();
+            $table->string('fee_hospital')->nullable();
+            $table->string('sub_total')->nullable();
+            $table->string('vat')->nullable();
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
