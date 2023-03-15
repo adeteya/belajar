b60f31bf8e57e2550a99bb7d763dd80bca87341d
adeteya <aditpermanaputra75@gmail.com>
2023-03-15 20:19:41 +0700

    Sampe Bisa Register JetStream

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/', function () {
 Route::get('/a', function () {
     return ('asd');
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
